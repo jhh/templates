0dcd2676782f3a069175b655ae20c9ae1b8fde39
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-08 22:36:57 -0500

    Move django files to correct dir

--- flake.nix
+++ flake.nix
@@ -1,38 +1,16 @@
 {
-  description = "Django application";
+  description = "Nix Flake Templates";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self, nixpkgs }: {
+    templates = {
+      django = {
+        path = ./django;
+        description = "A simple Django project";
+      };
+      html = {
+        path = ./html;
+        description = "HTML design setup";
+      };
     };
   };
-
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv mkPoetryApplication;
-      in
-      {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
-
-          devEnv = mkPoetryEnv {
-            projectDir = self;
-            groups = [ "main" "dev" ];
-          };
-
-          # refresh venv for Pycharm with: nix build .#venv -o .venv
-          venv = self.packages.${system}.devEnv;
-          default = self.packages.${system}.myapp;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.devEnv ];
-          packages = with pkgs; [ poetry ];
-        };
-      });
 }
